5b998ed2ce2be457ed61e83283f12c0e8bb749c5
Leif Keane <kdopo@udel.edu>
2024-04-13 15:53:43 -0400

    Added theme selection

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from "./page components/header/header.component";
 import { BodyComponent } from "./page components/body/body.component";
 import { AsideComponent } from "./page components/aside/aside.component";
 import { FooterComponent } from "./page components/footer/footer.component";
+import { OptionsComponent } from "./page components/options/options.component";
 //html file imports
 
 /**
@@ -19,6 +20,32 @@ export class MainComponent extends webez.EzComponent {
     private body: BodyComponent = new BodyComponent();
     private aside: AsideComponent = new AsideComponent();
     private footer: FooterComponent = new FooterComponent();
+    private options: OptionsComponent = new OptionsComponent();
+    //importing the main parts of the page
+
+    @webez.BindStyle("a", "backgroundColor")
+    backgroundA: string = "rgb(0, 255, 187)";
+    @webez.BindStyle("d", "backgroundColor")
+    backgroundD: string = this.backgroundA;
+    @webez.BindStyle("g", "backgroundColor")
+    backgroundG: string = this.backgroundA;
+    @webez.BindStyle("j", "backgroundColor")
+    backgroundJ: string = this.backgroundA;
+
+    @webez.BindStyle("b", "backgroundColor")
+    backgroundB: string = "rgb(217, 0, 255)";
+    @webez.BindStyle("e", "backgroundColor")
+    backgroundE: string = this.backgroundB;
+    @webez.BindStyle("h", "backgroundColor")
+    backgroundH: string = this.backgroundB;
+
+    @webez.BindStyle("c", "backgroundColor")
+    backgroundC: string = "rgb(255, 204, 0)";
+    @webez.BindStyle("f", "backgroundColor")
+    backgroundF: string = this.backgroundC;
+    @webez.BindStyle("i", "backgroundColor")
+    backgroundI: string = this.backgroundC;
+    //colors for moving background
 
     constructor() {
         super(html, css);
@@ -26,5 +53,21 @@ export class MainComponent extends webez.EzComponent {
         this.addComponent(this.body, "body");
         this.addComponent(this.aside, "aside");
         this.addComponent(this.footer, "footer");
+        this.addComponent(this.options, "options");
     } //constructor that adds all of the main elements of the page
+
+    @webez.Timer(0.1)
+    updateStyle() {
+        this.backgroundA = this.options.getThemeColors()[0];
+        this.backgroundB = this.options.getThemeColors()[1];
+        this.backgroundC = this.options.getThemeColors()[2];
+        //sets the background color variables to the theme colors
+        this.backgroundD = this.backgroundA;
+        this.backgroundG = this.backgroundA;
+        this.backgroundJ = this.backgroundA;
+        this.backgroundE = this.backgroundB;
+        this.backgroundH = this.backgroundB;
+        this.backgroundF = this.backgroundC;
+        this.backgroundI = this.backgroundC;
+    }
 }
--- src/app/page components/options/options.component.ts
+++ src/app/page components/options/options.component.ts
@@ -3,8 +3,53 @@ import css from "./options.component.css";
 import * as webez from "@gsilber/webez";
 //main functionality imports
 
-export class AsideComponent extends webez.EzComponent {
+export class OptionsComponent extends webez.EzComponent {
+    @webez.BindValue("theme-input")
+    themeSelect: string = "cmyk";
+
+    private themeColors: string[] = [
+        "rgb(0, 255, 187)",
+        "rgb(217, 0, 255)",
+        "rgb(255, 204, 0)",
+    ];
+
     constructor() {
         super(html, css);
     } //basic constructor
+
+    @webez.Change("theme-input")
+    private onThemeChange(v: webez.ValueEvent) {
+        this.themeSelect = v.value;
+        let themeColors: string[] = [
+            "rgb(0, 255, 187)",
+            "rgb(217, 0, 255)",
+            "rgb(255, 204, 0)",
+        ]; //definition of local variable
+
+        if (this.themeSelect === "cmyk") {
+            themeColors = [
+                "rgb(0, 255, 187)",
+                "rgb(217, 0, 255)",
+                "rgb(255, 204, 0)",
+            ]; //cmyk theme
+        } else if (this.themeSelect === "rgb") {
+            themeColors = [
+                "rgb(255, 75, 30)",
+                "rgb(90, 233, 80)",
+                "rgb(47, 90, 233)",
+            ]; //rgb theme
+        } else if (this.themeSelect === "trans") {
+            themeColors = [
+                "rgb(0, 171, 255)",
+                "rgb(255, 117, 255)",
+                "rgb(235, 242, 240)",
+            ]; //transgender theme
+        }
+
+        this.themeColors = themeColors;
+    }
+
+    public getThemeColors(): string[] {
+        return this.themeColors;
+    }
 }
